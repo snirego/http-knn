5a077675723d71a43ebc5d05cc113492b0a6ea14
Elior Dana <eliordana27@gmail.com>
2022-08-23 22:34:03 +0300

    first version

--- server.py
+++ server.py
@@ -4,18 +4,21 @@ from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "Hello World!"
-
 @app.route("/user/<user>")
 def user(user):
     return "<h1>Hello, {}!<h1/>".format(user)
 
-@app.route('/welcome')
+@app.route('/')
 def welcome():
-    return render_template('welcome.html')  # render a template
+    return render_template("welcome.html")  # render a template
 
+@app.route('/log_in')
+def log_in():
+    return render_template("log_in.html")
+    
+@app.route('/sing_up')
+def sing_up():
+    return render_template("sing_up.html")
 
 if __name__ == "__main__":
     # app.run(host="10.0.1.83")
