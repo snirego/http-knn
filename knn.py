4752e65664a9cdefea6434924f7fb813341e34ec
שניר אגו <snirego@Snirs-MacBook-Pro.local>
2022-08-23 17:55:37 +0300

    first commit

--- knn.py
+++ knn.py
@@ -1,4 +1,5 @@
 import numpy as np
+from sklearn.datasets import load_digits
 
 class KNN:
     def __init__(self, k=5):
@@ -21,13 +22,53 @@ class KNN:
         self.x_train = x_train
         self.y_train = y_train
 
-    # def predict(self, x_test):
-    #     for matrix in 
+    def predict(self, x):
+        y = None
+        distances_list = []
+        
+        if self.x_train == None:
+            raise Exception('Fit the model first!')
+        else:
+            i = 0
+            for row in self.x_train:
+                distances_list.append(((self.calculate_distance_between_two_arrays(x, row)), self.y_train[i]))
+                i += 1
+
+        # Now we have a list of all distances from our x (picture)
+        # We need the K lowest distances from the list
+        distances_list.sort(key=lambda t: t[0])
+        k_nearest_neighbours = distances_list[0:self.k]
+
+        # Now we need to find the most common class in the k nearest neighbours
+        d = {}
+        for tup in k_nearest_neighbours:
+            if tup[1] in d.keys():
+                d[tup[1]] += 1
+            else:
+                d[tup[1]] = 1
+
+        # Return the most common value in the dictionary
+        # sorted_dict = {}
+        # sorted_keys = sorted(d, key=d.get)
+
+        # for w in sorted_keys:
+        #     sorted_dict[w] = d[w]
+        sorted_dict = dict(sorted(d.items(), key=lambda item:item[1]))
+
+        return list(sorted_dict.items())[-1][0]
+
 
 
 knn = KNN(3)
 
-list1 = [1,2]
-list2 = [0,0]
+X = 0
+pic1 = [0,0,0,30,45,87,0,0,0]
+pic2 = [0,0,0,43,90,65,0,0,0]
+pic3 = [0,0,0,0,0,0,0,0,0]
+pic4 = [0,0,0,0,33,56,0,0,0]
+pic5 = [0,0,0,0,0,0,0,0,0]
+X = [pic1, pic2, pic3, pic4, pic5]
+Y = [1,2,'num 3',4,'num 3']
 
-print(knn.calculate_distance_between_two_arrays(list1,list2))
+knn.fit(X, Y)
+print('Prediction --->', knn.predict(pic5))
