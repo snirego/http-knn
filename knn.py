9e8b412a22aaeacfc6623e02a4cf6d0813284d6f
Snir <snirego75@gmail.com>
2022-08-25 16:32:21 +0300

    Final > Sign in/up NOT WORKING

--- knn.py
+++ knn.py
@@ -9,18 +9,18 @@ class KNN:
         self.y_pred = None
         self.k_nearest_neighbours_new = []
 
+
     def calculate_distance_between_two_points(self, point1, point2):
         return np.sqrt(np.sum((point1 - point2) ** 2))
 
+
     def calculate_distance_between_two_arrays(self, arr1, arr2):
         if len(arr1) != len(arr2):
+            print('Arr1 shape:', arr1.shape)
+            print('Arr2 shape:', arr2.shape)
             raise Exception("Arrays must be of the same length")
         else:
             return self.calculate_distance_between_two_points(arr1, arr2)
-            # distances_list = []
-            # for i in range(len(arr1)):
-            #     distances_list.append(self.calculate_distance_between_two_points(arr1[i], arr2[i]))
-            # return np.sqrt(np.sum(distances_list))
 
 
     def fit(self, x_train, y_train):
@@ -39,32 +39,25 @@ class KNN:
                 i += 1
         except Exception as e:
             print(f'Exeption in __predict_one --->', e)
-
-        # Now we have a list of all distances from our x (picture)
-        # We need the K lowest distances from the list
-        # distances_list.sort(key=lambda t: t[0])
-        # k_nearest_neighbours = distances_list[0:self.k]
-
-        # Now we need to find the most common class in the k nearest neighbours
-        # And then return the most common value in the dictionary
-        # d = {}
-        # for tup in k_nearest_neighbours:
-        #     if tup[1] in d.keys():
-        #         d[tup[1]] += 1
-        #     else:
-        #         d[tup[1]] = 1
-        # sorted_dict = dict(sorted(d.items(), key=lambda item:item[1]))
-        # temp_pred = list(sorted_dict.items())[-1][0]
         
         if len(distances_list) != 0:
             for i in range(self.k):
                 m = min(distances_list, key=lambda x: x[0])
                 self.k_nearest_neighbours_new.append(m[1])
                 distances_list.remove(m)
-                # if (i + 1) % self.k == 0:
-                #     print(self.k_nearest_neighbours_new)
             
-            res = max(set(self.k_nearest_neighbours_new), key=self.k_nearest_neighbours_new.count)
+            # res = max(set(self.k_nearest_neighbours_new), key=self.k_nearest_neighbours_new.count)
+
+            # print(f'k_nearest_neighbours_new --->', self.k_nearest_neighbours_new)
+            knn_list = []
+            for i in self.k_nearest_neighbours_new:
+                # convert each numpy.ndarray to int
+                i = int(i)
+                # print('Type i --->', type(i))
+                # print(f'i --->', i)
+                knn_list.append(i)
+            # print(f'k_nearest_neighbours_new ---< 2 >--- --->', self.k_nearest_neighbours_new)
+            res = max(set(knn_list), key=knn_list.count)
         
         else:
             res = None
@@ -85,8 +78,6 @@ class KNN:
 
     def accuracy(self, y_test):
         try:
-            # Actuall = y_test
-            # Predicted = self.y_pred
             correct = 0
             for i in range(len(y_test)):
                 if y_test[i] == self.y_pred[i]:
@@ -94,30 +85,3 @@ class KNN:
             return correct / len(y_test)
         except Exception as e:
             print(f'Exeption in accuracy --->', e)
-
-# knn = KNN(3)
-
-# digits = load_digits()
-
-# X = digits.data
-# Y = digits.target
-
-# x_train = X[:1400]
-# y_train = Y[:1400]
-# x_test = X[1400:]
-# y_test = Y[1400:]
-
-# time_start = time.perf_counter()
-
-# knn.fit(x_train, y_train)
-
-# knn.predict(x_test)
-
-# # print('Prediction --->', knn.predict(x_test))
-# # print('Actual --->', y_test)
-
-# time_end = time.perf_counter()
-
-# print("Time to fit the model: {}".format(time_end - time_start))
-
-# print("Accuracy: {}".format(knn.accuracy(y_test)))
--- server.py
+++ server.py
@@ -1,16 +1,14 @@
-from curses import window
 from flask import Flask, render_template
 import base64
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, request
 from sklearn.datasets import load_digits
-from sklearn.svm import SVC
-from sklearn.ensemble import RandomForestClassifier
-from xgboost import XGBClassifier
-# from keras.datasets import mnist
 
 from knn import KNN
 import cv2
 import numpy as np
+import json
+import io
+import os
 
 app = Flask(__name__)
 
@@ -18,7 +16,7 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
-@app.route("/draw")
+@app.route("/draw", methods=['GET', 'POST'])
 def draw():
     return render_template("draw.html")
 
@@ -26,13 +24,44 @@ def draw():
 def pred(prediction=None):
     return render_template("pred.html", pred=prediction)
 
-@app.route('/signup')
-def signup():
-    return render_template('sign_up.html')  # render a template
+# @app.route('/signup')
+# def signup():
+#     return render_template('signup.html')  # render a template
 
-@app.route('/signin')
+# @app.route('/signin')
+# def signin():
+#     return render_template('signin.html')  # render a template
+
+@app.route('/signup', methods = ['GET','POST'])
+def signup():
+
+    try:
+        with open('users.json', "w") as file:
+            dictionary = json.load(file)
+            print(dictionary)
+    except:
+        print("File not found in signup")
+    return render_template('signup.html')
+
+@app.route('/signin', methods=['GET','POST'])
 def signin():
-    return render_template('sign_in.html')  # render a template
+
+    try:
+        with open('users.json', "r") as file:
+            dictionary = json.load(file)
+            print(dictionary)
+            if request.method == 'POST':  
+                username = request.form["username"]
+                password = request.form["password"]  
+                for key,value in dictionary.items():
+                    if key == username and value == password:
+                        return render_template("draw.html")
+                    else:
+                        return render_template("signup.html")
+    except:
+        print("File not found in signin")
+    return render_template('signin.html')
+
 
 @app.route('/output', methods=['POST'])
 def output():
@@ -40,53 +69,91 @@ def output():
     image_to_predict = data['img_base64'] #will give you array a
 
     base64_img = image_to_predict.replace('data:image/png;base64,', '')
-    # print((image_to_predict))
 
+    # Decode the base64 string image
     base64_img_bytes = base64_img.encode('utf-8')
     with open('decoded_image.png', 'wb') as file_to_save:
         decoded_image_data = base64.decodebytes(base64_img_bytes)
         file_to_save.write(decoded_image_data)
 
+    # Dealing with the image
     image = cv2.imread('decoded_image.png')
 
+    # Invert the image colors so it will be similar to the sklearn.load_digits dataset
     invert = cv2.bitwise_not(image)
     invert_two = cv2.bitwise_not(invert)
-    
     cv2.imwrite('inverted_image.png', invert_two)
 
-    resized = cv2.resize(invert_two, (8,8), interpolation = cv2.INTER_AREA)
 
-    resized = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
- 
-    # print('Resized Dimensions : ',resized.shape)
+    # Resize the image to 8x8 pixels like the sklearn.load_digits dataset
+    # resized = cv2.resize(invert_two, (8,8), interpolation = cv2.INTER_AREA)
     
+    # Resize the image to 20x20 pixels like the dataset
+    resized = cv2.resize(invert_two, (20,20), interpolation = cv2.INTER_AREA)
+    resized = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)    
     cv2.imwrite('resized_image.png', resized)
 
-    # Start KNN Prediction
+
+# region Old code
+    # # Start KNN Prediction
+    # knn = KNN(3)
+
+    # digits = load_digits()
+    # x_train = digits.data[0:1400]
+    # y_train = digits.target[0:1400]
+    # x_test = digits.data[1400:]
+    # y_test = digits.target[1400:]
+
+
+    # knn.fit(x_train, y_train)
+    # print('Our KNN Prediction --->', knn.predict(resized.reshape(1, -1)))
+
+# endregion
+
+    # ----------------------------------------------------------------------------
+    # NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW NEW
+    # ----------------------------------------------------------------------------
+    # Read the image
+    data_image = cv2.imread('digits.png')
+    
+    # gray scale conversion
+    gray_img = cv2.cvtColor(data_image, cv2.COLOR_BGR2GRAY)
+    
+    # We will divide the image
+    # into 5000 small dimensions 
+    # of size 20x20
+    divisions = list(np.hsplit(i,100) for i in np.vsplit(gray_img,50))
+    
+    # Convert into Numpy array
+    # of size (50,100,20,20)
+    NP_array = np.array(divisions)
+    
+    # Preparing train_data
+    # and test_data.
+    # Size will be (2500,20x20)
+    x_train = NP_array[:,:50].reshape(-1,400).astype(np.float32)
+    
+    # Size will be (2500,20x20)
+    x_test = NP_array[:,50:100].reshape(-1,400).astype(np.float32)
+    
+    # Create 10 different labels 
+    # for each type of digit
+    k = np.arange(10)
+    y_train = np.repeat(k,250)[:,np.newaxis]
+    y_test = np.repeat(k,250)[:,np.newaxis]
+    
+    # Initiate kNN classifier
     knn = KNN(3)
-
-    digits = load_digits()
-    x_train = digits.data[0:1400]
-    y_train = digits.target[0:1400]
-    x_test = digits.data[1400:]
-    y_test = digits.target[1400:]
-
+    
+    # Train the classifier
     knn.fit(x_train, y_train)
     print('Our KNN Prediction --->', knn.predict(resized.reshape(1, -1)))
+    # ----------------------------------------------------------------------------
 
-    svm = SVC()
-    svm.fit(x_train, y_train)
-    print('SVM Prediction --->', svm.predict(resized.reshape(1, -1)))
 
-    rf = RandomForestClassifier()
-    rf.fit(x_train, y_train)
-    print('Random Forest Prediction --->', rf.predict(resized.reshape(1, -1)))
 
-    xgb = XGBClassifier()
-    xgb.fit(x_train, y_train)
-    print('XGBoost Prediction --->', xgb.predict(resized.reshape(1, -1)))
 
-    
+
     # return render_template('pred.html', pred=knn.predict(resized.reshape(1, -1)))
     return '<h1> Prediction Header </h1>'
 
